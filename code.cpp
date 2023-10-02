344c28ada70959bed46dac2e27302f4ec15ea906
Prasad Chathuranga <46666566+Prasad-Chathuranga@users.noreply.github.com>
2023-10-02 15:43:02 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main()
 {
 
- float pay,dis;
+ float pay,discount;
 
  cout << "Enter Payment amount : ";
  cin >> pay;
 
     if (pay > 10000)
     {
-      discount=p*25/100;
+      discount=pay*25/100;
     }
 
     else if (pay > 5000)
     {
-      discount=y*15/100;
+      discount=pay*15/100;
     }
 
     else if (pay > 3000)
@@ -27,6 +27,6 @@ int main()
     else
     discount= 0;
 
- cout << "Discount  is -> " << dis << endl;
+ cout << "Discount  is -> " << discount << endl;
 return 0;
 }
